4bf22cb7d382206824e9418aaf2fc1f20091382a
Mizuki <m.minegishi.sys23@morijyobi.ac.jp>
2024-07-10 10:30:33 +0900

    sprinti完了 closed #6#7#8

---
+++ src/player.java
@@ -0,0 +1,24 @@
+package src;
+public class player {
+
+    private String playerName;
+
+    public player(String playerName) {
+
+        this.playerName = playerName;
+
+    }
+
+    public String getPlayerName() {
+
+        return playerName;
+
+    }
+
+    public void setPlayerName(String playerName) {
+
+        this.playerName = playerName;
+
+    }
+
+}
